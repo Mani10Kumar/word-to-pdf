b1185fe19b623f12e2218c9ae44bb324879f8d74
Mani Kumar <manispacekumar@gmail.com>
2025-12-03 13:21:00 +0530

    libreoffice comment off

--- src/main/java/com/pdf/word/LibreOfficeConfig.java
+++ src/main/java/com/pdf/word/LibreOfficeConfig.java
@@ -1,28 +1,20 @@
-package com.pdf.word;
-
-import org.jodconverter.core.office.OfficeManager;
-import org.jodconverter.local.office.LocalOfficeManager;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class LibreOfficeConfig {
-
-	@Bean(initMethod = "start", destroyMethod = "stop")
-	public OfficeManager officeManager() {
-	    return LocalOfficeManager.builder()
-	            .officeHome("C:\\Program Files\\LibreOffice")
-	            .portNumbers(2002)
-	            .install()
-	            .build();
-	}
-
-//    @Bean(destroyMethod = "stop")
-//    public OfficeManager officeManager() {
-//        return LocalOfficeManager.builder()
-//                .officeHome("C:\\Program Files\\LibreOffice")
-//                .portNumbers(2002)
-//                .install()    // IMPORTANT
-//                .build();
-//    }
-}
+//package com.pdf.word;
+//
+//import org.jodconverter.core.office.OfficeManager;
+//import org.jodconverter.local.office.LocalOfficeManager;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class LibreOfficeConfig {
+//
+//	@Bean(initMethod = "start", destroyMethod = "stop")
+//	public OfficeManager officeManager() {
+//	    return LocalOfficeManager.builder()
+//	            .officeHome("C:\\Program Files\\LibreOffice")
+//	            .portNumbers(2002)
+//	            .install()
+//	            .build();
+//	}
+//
+//}
